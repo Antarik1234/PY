42a2bac503ed1fc1ae5a58ef5a259137bdbd23e4
Antarik Sinha <120300062+Antarik1234@users.noreply.github.com>
2025-01-09 05:02:57 +0000

    code2

---
+++ 01.py
@@ -0,0 +1,15 @@
+class Student:
+    pass
+    
+std1=Student()
+std2=Student()
+
+std1.fName='Anwar'
+std1.lName='Ali'
+std1.reg='MCT-UET-01'
+std2.fName='Akbar'
+std2.lName='Khan'
+std2.reg='MCT-UET-02'
+
+print(std1.fName)
+print(std2.reg)
---
+++ 02.py
@@ -0,0 +1,15 @@
+class Student:
+    def initial(obj,f,l,r):
+        obj.fName=f
+        obj.lName=l
+        obj.reg=r
+        
+    
+std1=Student()
+std2=Student()
+
+std1.initial('Amar','Ali','MCT-UET-01')
+std2.initial('Akbar','Khan','MCT-UET-02')
+
+print(std1.fName)
+print(std2.reg)
